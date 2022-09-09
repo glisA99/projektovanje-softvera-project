6aafc24f4d487d90f3e089d333d340ae25766d2b
glisA99 <ognjen.simic99@gmail.com>
2022-09-09 18:13:13 +0200

    Fixed bug with logged radnik data not being saved in clinet thread

--- Server/src/threads/ClientThreadImpl.java
+++ Server/src/threads/ClientThreadImpl.java
@@ -1,5 +1,6 @@
 package threads;
 
+import communication.Operations;
 import communication.Receiver;
 import communication.Request;
 import communication.Response;
@@ -20,12 +21,12 @@ import login.so.LoginSO;
 public class ClientThreadImpl extends ClientThread {
 
     public ClientThreadImpl(Socket socket, ServerThread serverThread) {
-        super(socket,serverThread);
+        super(socket, serverThread);
     }
 
     @Override
     public void run() {
-        while(!this.socket.isClosed()) {
+        while (!this.socket.isClosed()) {
             try {
                 Request request = null;
                 try {
@@ -37,14 +38,7 @@ public class ClientThreadImpl extends ClientThread {
                 System.out.println("Receiver request from client... Operation type: " + request.getOperation());
                 Response response = handleRequest(request);
                 new Sender(socket).send(response);
-                // add statistic
-                if (response.getResponseType().equals(ResponseType.SUCCESS)) {
-                    this.clientStatistic.addRequest(ResponseType.SUCCESS);
-                    System.out.println("Client request handled sucessfully...");
-                } else {
-                    System.out.println("Client request failed...");
-                    this.clientStatistic.addRequest(ResponseType.FAILURE);
-                }
+                processRequestForStatistics(request, response);
             } catch (Exception ex) {
                 try {
                     this.socket.close();
@@ -62,23 +56,26 @@ public class ClientThreadImpl extends ClientThread {
 
     private Response handleRequest(Request request) {
         int operation = request.getOperation();
-        
-        switch(operation) {
-            case LOGIN: return handleLogin(request);
+
+        switch (operation) {
+            case LOGIN:
+                return handleLogin(request);
         }
-        
+
         return null;
     }
 
     private Response handleLogin(Request request) {
         Response response = new Response();
         Radnik radnik = (Radnik) request.getData();
-        
+
         try {
             LoginSO so = new LoginSO();
             so.execute(radnik);
             Radnik radnik1 = (Radnik) so.operationResult;
-            if (radnik1 == null) throw new Exception("Radnik not found");
+            if (radnik1 == null) {
+                throw new Exception("Radnik not found");
+            }
             response.setResponseType(ResponseType.SUCCESS);
             response.setResponse(radnik1);
         } catch (Exception ex) {
@@ -87,8 +84,23 @@ public class ClientThreadImpl extends ClientThread {
             response.setResponseType(ResponseType.FAILURE);
             response.setException(ex);
         }
-        
+
         return response;
     }
-    
+
+    private void processRequestForStatistics(Request request, Response response) {
+        int operation = request.getOperation();
+        // add statistic
+        if (response.getResponseType().equals(ResponseType.SUCCESS)) {
+            this.clientStatistic.addRequest(ResponseType.SUCCESS);
+            Radnik radnik = (Radnik) response.getResponse();
+            // set logged radnik
+            if (operation == Operations.LOGIN) this.clientStatistic.setLoggedRadnik(radnik);
+            System.out.println("Client request handled sucessfully...");
+        } else {
+            System.out.println("Client request failed...");
+            this.clientStatistic.addRequest(ResponseType.FAILURE);
+        }
+    }
+
 }
