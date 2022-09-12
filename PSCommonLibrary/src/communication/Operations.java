6e600a6ab4e3d0322dfe59cc57b873dbda743af8
glisA99 <ognjen.simic99@gmail.com>
2022-09-12 19:37:44 +0200

    Added remaining operations

--- PSCommonLibrary/src/communication/Operations.java
+++ PSCommonLibrary/src/communication/Operations.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package communication;
 
 /**
@@ -12,9 +7,28 @@ package communication;
 public class Operations {
     
     public static final int LOGIN = 1;
+    
+    // operations on client
     public static final int GET_ALL_CLIENTS = 2;
     public static final int GET_CLIENTS_CONDITIONAL = 3;
     public static final int CREATE_CLIENT = 4;
     public static final int SAVE_CLIENT = 5;
+    
+    // operations on artikl
+    public static final int DELETE_ARTIKL = 6;
+    public static final int CREATE_ARTIKL = 7;
+    public static final int SAVE_ARTIKL = 8;
+    public static final int FIND_ARTIKL = 9;
+    public static final int SEARCH_ARTIKLS = 10;
+    
+    // operations on prodajna stavka
+    public static final int DELETE_PRODAJNA_STAVKA = 11;
+    public static final int CREATE_PRODAJNA_STAVKA = 12;
+    public static final int SAVE_PRODAJNA_STAVKA = 13;
+    public static final int SEARCH_PRODAJNE_STAVKE = 14;
+    
+    // operations on izvestaj
+    public static final int CREATE_IZVESTAJ = 15;
+    public static final int SAVE_IZVESTAJ = 16;
     
 }
